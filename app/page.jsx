242be1808fba229d1c91c4ab973e7b931c607814
InfectiousAlpha <nurmaghribii@gmail.com>
2026-02-19 18:53:26 +0700

    Create page.jsx

---
+++ app/page.jsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div className="content-container">
+      <h1 style={{fontSize: '4rem', fontWeight: 'bold'}}>you login</h1>
+      <p style={{marginTop: '20px'}}>Welcome to your private dashboard.</p>
+    </div>
+  );
+}
