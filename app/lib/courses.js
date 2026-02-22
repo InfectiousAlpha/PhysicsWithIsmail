e58307686311ab56e0b092fb7ad853bb9eec169d
InfectiousAlpha <nurmaghribii@gmail.com>
2026-02-22 15:44:29 +0700

    Create courses.js

---
+++ app/lib/courses.js
@@ -0,0 +1,23 @@
+export const courses = [
+  {
+    id: '1',
+    title: 'Course 1: Introduction to Physics',
+    description: 'Learn the fundamentals of motion, speed, and classical mechanics.',
+    requiredLevel: 0,
+    unlocksLevel: 1
+  },
+  {
+    id: '2',
+    title: 'Course 2: Thermodynamics & Waves',
+    description: 'Understand heat transfer, energy systems, and basic wave properties.',
+    requiredLevel: 1,
+    unlocksLevel: 2
+  },
+  {
+    id: '3',
+    title: 'Course 3: Quantum Physics Basics',
+    description: 'Dive into the fascinating and weird world of subatomic particles.',
+    requiredLevel: 2,
+    unlocksLevel: 3
+  }
+];
