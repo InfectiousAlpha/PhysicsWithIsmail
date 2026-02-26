9b5d6647416c06ce6e2e83974dcf6eb8c4b2e522
InfectiousAlpha <nurmaghribii@gmail.com>
2026-02-26 18:30:03 +0700

    Delete app/components/simulations directory

--- app/components/simulations/course3/sim2.jsx
+++
@@ -1,9 +0,0 @@
-'use client';
-
-// Repeating the multi particle for Course 3 based on your original page logic mapping
-import Course2Sim1 from '../course2/sim1';
-
-export default function Course3Sim2({ simId }) {
-  // We can reuse the same component internally or you can modify it as needed later
-  return <Course2Sim1 simId={simId} />;
-}
--- app/components/simulations/coursem4/sim1.jsx
+++
@@ -1,6 +0,0 @@
-'use client';
-import Calculator from '../math/Calculator';
-
-export default function CourseM4Sim1({ simId }) {
-  return <Calculator simId={simId} />;
-}
